6e1d6966188229cb4b4b7a82ea9605595a8ce552
twtc <raner.piibur@gmail.com>
2017-06-14 22:55:22 +0300

    Added echos.

--- build-pi.sh
+++ build-pi.sh
@@ -2,6 +2,11 @@
 
 source settings.sh
 
+echo Building project...
 cargo build --target=$TARGET
+
+echo Syncing with PI...
 rsync -ru --delete ./target/$TARGET pi@$IP:/home/pi/soulful-companion/target
+
+echo Running on PI.
 ssh pi@$IP "/home/pi/soulful-companion/target/$TARGET/debug/soulful-companion"
