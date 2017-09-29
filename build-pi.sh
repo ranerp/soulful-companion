a6b904c7569d5846bc604be9c734d313f4358823
Raner Piibur <raner.piibur@gmail.com>
2017-09-29 20:34:51 +0300

    Changed ssh command go to the root directory of the project.

--- build-pi.sh
+++ build-pi.sh
@@ -9,4 +9,4 @@ echo Syncing with PI...
 rsync -ru --delete ./target/$TARGET pi@$IP:/home/pi/soulful-companion/target
 
 echo Running on PI.
-ssh pi@$IP "/home/pi/soulful-companion/target/$TARGET/debug/soulful-companion"
+ssh pi@$IP "cd /home/pi/soulful-companion; target/$TARGET/debug/soulful-companion"
